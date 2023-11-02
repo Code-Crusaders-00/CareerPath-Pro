423f7c1f72fb926ee0477847537d412359db272f
Aidan <aifl8988@colorado.edu>
2023-11-02 16:00:31 -0600

    Create.sql Version 1 - Aidan

---
+++ Project Code/init_data/create.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+  username VARCHAR(50) PRIMARY KEY,
+  password CHAR(60) NOT NULL
+  firstName VARCHAR,
+  lastNAME VARCHAR,
+  email VARCHAR
+);
+
+DROP TABLE IF EXISTS applications CASCADE;
+CREATE TABLE applications(
+    jobID INTEGER PRIMARY KEY,
+    name VARCHAR,
+    company VARCHAR,
+    industry VARCHAR,
+    description VARCHAR
+)
+
+DROP TABLE IF EXISTS jobs_to_user CASCADE;
+CREATE TABLE jobs_to_user(
+    jobID INTEGER ,
+    username VARCHAR(50)
+);
