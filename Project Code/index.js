178f1ba0b87e327734e32dd27543ea10dbd74f1f
ryanm <rymo1472@colorado.edu>
2023-11-09 13:41:00 -0700

    Register and login backend

--- Project Code/index.js
+++ Project Code/index.js
@@ -7,23 +7,23 @@ const bcrypt = require('bcrypt'); //  To hash passwords
 
 // TO DO: Connect to database stuff
 const dbConfig = {
-  host: 'db', // the database server
-  port: 5432, // the database port
-  database: process.env.POSTGRES_DB, // the database name
-  user: process.env.POSTGRES_USER, // the user account to connect with
-  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
 };
 const db = pgp(dbConfig);
 
 // test your database
 db.connect()
-  .then(obj => {
-    console.log('Database connection successful'); // you can view this message in the docker compose logs
-    obj.done(); // success, release the connection;
-  })
-  .catch(error => {
-    console.log('ERROR:', error.message || error);
-  });
+    .then(obj => {
+        console.log('Database connection successful'); // you can view this message in the docker compose logs
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
 
 app.set('view engine', 'ejs'); // set the view engine to EJS
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
@@ -38,87 +38,112 @@ app.use(bodyParser.json()); // specify the usage of JSON for parsing request bod
 // );
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 // TO DO: API endpoints
 app.get('/', (req, res) => {
-  res.redirect('/login');
+    res.redirect('/login');
 });
 
 app.get('/login', (req, res) => {
-  res.render('pages/login');
+    res.render('pages/login');
 });
 
 app.post('/login', (req, res) => {
 
 });
 
-app.get('/users/:userId/job-applications', (req, res) => {
-  const queryOne = `SELECT jobID FROM jobs_to_user WHERE username = '${req.params.userId}'`;
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+});
 
-  db.task(t => {
-      return t.any(queryOne)
-        .then(jobId => {
-          const jobIds = jobId.map( (job) => {
-            return job.jobid;
-          });
-          const queryTwo = `SELECT * FROM applications WHERE jobID = ANY (array[${jobIds}])`;
-          return t.any(queryTwo);
+app.post('/register', async (req, res) => {
+    const hash = await bcrypt.hash(req.body.password, 10);
+    const query = `INSERT INTO users (username, password, firstName, lastName, email)
+                   VALUES ($1, $2, $3, $4, $5)`
+    try {
+        await db.none(query, [req.body.username, hash, req.body.first_name, req.body.last_name, req.body.email]);
+        console.log(`Registered user ${req.body.username}`)
+        res.redirect('/login');
+    } catch (error) {
+        console.log(`Failed to register user: ${error}`);
+        res.redirect('/register');
+    }
+})
+
+app.get('/users/:userId/job-applications', (req, res) => {
+    const queryOne = `SELECT jobID
+                      FROM jobs_to_user
+                      WHERE username = '${req.params.userId}'`;
+
+    db.task(t => {
+        return t.any(queryOne)
+            .then(jobId => {
+                const jobIds = jobId.map((job) => {
+                    return job.jobid;
+                });
+                const queryTwo = `SELECT *
+                                  FROM applications
+                                  WHERE jobID = ANY (array [${jobIds}])`;
+                return t.any(queryTwo);
+            });
+    })
+        .then((jobs) => {
+            res.send(jobs);
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .then( (jobs) => {
-      res.send(jobs);
-    })
-    .catch( (err) => {
-      console.log(err);
-    });
 });
 
 app.post('/users/:userId/job-applications', (req, res) => {
-  const queryOne = `INSERT INTO applications
-                (name, company, industry, description)
-                VALUES
-                ('${req.body.name}','${req.body.company}','${req.body.industry}','${req.body.description}') RETURNING jobID`;
+    const queryOne = `INSERT INTO applications
+                          (name, company, industry, description)
+                      VALUES ('${req.body.name}', '${req.body.company}', '${req.body.industry}',
+                              '${req.body.description}')
+                      RETURNING jobID`;
 
-  db.one(queryOne)
-    .then( (jobId) => {
-      const queryTwo = `INSERT INTO jobs_to_user (jobID, username)
-                        VALUES (${jobId}, '${req.params.userId}')`;
+    db.one(queryOne)
+        .then((jobId) => {
+            const queryTwo = `INSERT INTO jobs_to_user (jobID, username)
+                              VALUES (${jobId}, '${req.params.userId}')`;
 
-      db.none(queryTwo)
-        .then( () => {
-          res.status(200);
-          res.redirect(`/users/${req.params.userId}/job-applications`);
+            db.none(queryTwo)
+                .then(() => {
+                    res.status(200);
+                    res.redirect(`/users/${req.params.userId}/job-applications`);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         })
-        .catch( (err) => {
-          console.log(err);
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .catch( (err) => {
-      console.log(err);
-    });
 });
 
 app.get('/users/:userId/job-applications/:applicationId', (req, res) => {
-  const query = `SELECT * FROM applications WHERE jobID = '${req.params.applicationId}'`;
+    const query = `SELECT *
+                   FROM applications
+                   WHERE jobID = '${req.params.applicationId}'`;
 
-  db.one(query)
-    .then( (job) => {
-      res.send(job);
-    })
-    .catch( (err) => {
-      console.log(err);
-    });
+    db.one(query)
+        .then((job) => {
+            res.send(job);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
 
 app.get('/jobs', (req, res) => {
-  res.render('pages/jobBoard');
+    res.render('pages/jobBoard');
 });
