0db3d881939d24dac674100de06c8514563e86e4
Navan Chauhan <navanchauhan@gmail.com>
2023-11-15 13:51:34 -0700

    add jobs for you preview

--- Project Code/index.js
+++ Project Code/index.js
@@ -4,6 +4,8 @@ const pgp = require('pg-promise')(); // To connect to the Postgres DB from the n
 const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcrypt'); //  To hash passwords
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 // TO DO: Connect to database stuff
 const dbConfig = {
@@ -43,7 +45,8 @@ app.use(
     })
 );
 
-// TO DO: API endpoints
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get('/', (req, res) => {
     res.redirect('/login');
 });
@@ -74,11 +77,99 @@ app.post('/register', async (req, res) => {
     }
 })
 
-// READ all jobs with optional filtering
+/**
+ * @swagger
+ * /api/jobs:
+ *   get:
+ *     summary: Request a list of jobs with optional filtering
+ *     description: Retrieve a list of jobs with the ability to filter by various criteria.
+ *     tags: [Jobs]
+ *     parameters:
+ *       - in: query
+ *         name: company
+ *         schema:
+ *           type: string
+ *         description: Optional company name to filter jobs.
+ *       - in: query
+ *         name: role
+ *         schema:
+ *           type: string
+ *         description: Optional role to filter jobs.
+ *       - in: query
+ *         name: location
+ *         schema:
+ *           type: string
+ *         description: Optional location to filter jobs.
+ *       - in: query
+ *         name: offers_sponsorship
+ *         schema:
+ *           type: boolean
+ *         description: Optional filter for jobs offering sponsorship (true or false).
+ *       - in: query
+ *         name: requires_us_citizenship
+ *         schema:
+ *           type: boolean
+ *         description: Optional filter for jobs requiring U.S. citizenship (true or false).
+ *       - in: query
+ *         name: internship
+ *         schema:
+ *           type: boolean
+ *         description: Optional filter for internships (true or false).
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: number
+ *         description: Optional limit on the number of results returned (max 1000).
+ *       - in: query
+ *         name: random
+ *         schema:
+ *           type: boolean
+ *         description: Optional flag to return results in random order.
+ *     responses:
+ *       200:
+ *         description: List of jobs.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   company:
+ *                     type: string
+ *                     description: Company name of the job.
+ *                   role:
+ *                     type: string
+ *                     description: Role of the job.
+ *                   location:
+ *                     type: string
+ *                     description: Location of the job.
+ *                   offers_sponsorship:
+ *                     type: boolean
+ *                     description: Indicates if the job offers sponsorship.
+ *                   requires_us_citizenship:
+ *                     type: boolean
+ *                     description: Indicates if the job requires U.S. citizenship.
+ *                   internship:
+ *                     type: boolean
+ *                     description: Indicates if the job is an internship.
+ *         examples:
+ *           application/json:
+ *             [
+ *               {
+ *                 "company": "ExampleCorp",
+ *                 "role": "Software Engineer",
+ *                 "location": "New York",
+ *                 "offers_sponsorship": true,
+ *                 "requires_us_citizenship": false,
+ *                 "internship": false
+ *               }
+ *             ]
+ */
 app.get('/api/jobs', async (req, res) => {
     let count = 5;
     try {
-        const { company, role, location, offers_sponsorship, requires_us_citizenship, internship, limit} = req.query;
+        const { company, role, location, offers_sponsorship, requires_us_citizenship, internship, limit, random} = req.query;
         let query = 'SELECT * FROM jobs';
         const conditions = [];
         const parameters = [];
@@ -123,9 +214,13 @@ app.get('/api/jobs', async (req, res) => {
             }
         }
 
+        // Add random
+        if (random != null) {
+            query += ' ORDER BY RANDOM()';
+        }
+
+
         query += ` LIMIT ${count}`;
-
-
         const jobs = await db.any(query, parameters);
         res.status(200).json(jobs);
     } catch (error) {
@@ -247,8 +342,17 @@ const auth = (req, res, next) => {
 // Authentication Required
 app.use(auth);
 
-app.get('/home', (req, res) => {
-    res.render('pages/home', {user: req.session.user, error: req.session.error });
+app.get('/home', async (req, res) => {
+    // Get Random Jobs
+    try { 
+        const jobs = await fetch("http://localhost:3000/api/jobs?limit=15&random=true").then((res) =>
+        res.json()
+    );
+    res.render('pages/home', {user: req.session.user, error: req.session.error, jobs });
+    } catch (error) {
+        console.log(error);
+        res.render('pages/home', {user: req.session.user, error: req.session.error, jobs: [] });
+    }
 });
 
   app.get('/logout', (req, res) => {
